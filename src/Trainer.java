53175b951b389aeaf4f4c1cf902116b2e350cda7
Mihir Sharma <20mihirs@students.harker.org>
2019-12-12 22:39:25 -0800

    Fixed imports for speedier runtimes.

--- src/Trainer.java
+++ src/Trainer.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
